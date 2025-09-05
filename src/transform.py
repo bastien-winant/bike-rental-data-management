c222b3ee564c0b9b8cd745e9730507aa066ef49f
bastien-winant <armand.winant@gmail.com>
2025-09-05 21:13:18 +0200

    weather data transform function

--- src/transform.py
+++ src/transform.py
@@ -1,12 +1,22 @@
 import pandas as pd
-from src.utils import get_data_path
+from src.utils import read_raw_file, save_processed_file
 
-def load_rentals():
-	weather_data_path = get_data_path("raw", "newark_airport_2016.csv")
-	df = pd.read_csv(weather_data_path)
-	return df
+def load_weather():
+	# read in the raw data
+	df = read_raw_file("newark_airport_2016.csv")
+
+	# remove columns with all missing values
+	df.dropna(axis=1, inplace=True, how='all')
+
+	# convert date strings to datetime objects
+	df.DATE = pd.to_datetime(df.DATE)
+
+	df_station = df[['STATION', 'NAME']].drop_duplicates(ignore_index=True)
+	save_processed_file(df_station, 'stations.csv')
+
+	df.drop('NAME', axis=1, inplace=True)
+	save_processed_file(df, 'weather.csv')
 
 
 if __name__ == "__main__":
-	rentals = load_rentals()
-	print(rentals.head())
+	load_weather()
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,41 @@
 from pathlib import Path
+import pandas as pd
+
 
 def get_project_root() -> Path:
 	"""Return project root directory (two levels up from this file)."""
 	return Path(__file__).resolve().parents[1]
 
+
 def get_data_path(subdir: str, filename: str) -> Path:
 	"""
 	Build a path to a file in the data/ directory.
 	Example: get_data_path("raw", "bike_rentals.csv")
+	:param subdir: name of the folder that holds the data file
+	:param filename: name of the data file
+	:return: relative path to the data file
 	"""
 	return get_project_root() / "data" / subdir / filename
+
+
+def read_raw_file(filename: str) -> pd.DataFrame:
+	"""
+	Read in a file in the data/raw/ directory as a Pandas Dataframe
+	:param filename: name of the data file
+	:return: Pandas Dataframe
+	"""
+	file_path = get_data_path(subdir='raw', filename=filename)
+	df = pd.read_csv(file_path)
+
+	return df
+
+
+def save_processed_file(df: pd.DataFrame, filename: str):
+	"""
+	Save a Pandas DataFrame as CSV to the data/processed/ directory
+	:param df: Pandas DataFrame
+	:param filename: name of the data file
+	:return:
+	"""
+	file_path = get_data_path(subdir='processed', filename=filename)
+	df.to_csv(file_path, index=False)
